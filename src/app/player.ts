2250bae3ba10c445b1bb052561f697e45aa55b8a
unknown <songyanghan+git@gmail.com>
2022-01-27 19:39:57 +0800

    initial commit

---
+++ src/app/player.ts
@@ -0,0 +1,9 @@
+import { Prediction } from "./prediction";
+
+export class Player {
+    constructor(
+        public predictions:Prediction[]
+    ) {
+
+    }
+}
---
+++ src/app/prediction.ts
@@ -0,0 +1,8 @@
+export class Prediction{
+    constructor(
+        public guess:string,
+        public result:string
+    ) {
+
+    }
+}
