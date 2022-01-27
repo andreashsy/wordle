b07b24fdfa225c4013961f7df411ca970b04e31a
unknown <songyanghan+git@gmail.com>
2022-01-28 01:40:12 +0800

    EDIT: standarised guesses to lower case

--- src/app/word-not-in-list.directive.ts
+++ src/app/word-not-in-list.directive.ts
@@ -5,7 +5,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 /** The word must have n letters */
 export function wordNotInListValidator(stringList: String[]): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const notInList = !stringList.includes(control.value); 
+    const notInList = !stringList.includes(control.value.toLowerCase()); 
     return notInList ? {wordNotInList: {value: control.value}} : null;
   };
 }
