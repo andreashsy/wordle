4c2e29e779e5540edc0baa6e6a53cdc71a194106
unknown <songyanghan+git@gmail.com>
2022-01-29 20:15:04 +0800

    EDIT: fixed bug in statistics

--- src/app/statistics.ts
+++ src/app/statistics.ts
@@ -5,6 +5,7 @@ export class Statistics {
     winRate: number;
     totalNumberOfGuesses: number;
     averageNumberOfGuesses: number;
+    averageNumberOfGuessesInWonGames: number;
 
     constructor(
         rawData: string
@@ -15,6 +16,7 @@ export class Statistics {
         this.totalNumberOfGuesses = this.getTotalGuesses(rawData)
         this.winRate = Math.round((this.numberOfWins * 100 / this.totalGamesPlayed) * 10) / 10
         this.averageNumberOfGuesses = Math.round((this.totalNumberOfGuesses / this.totalGamesPlayed) * 100) / 100
+        this.averageNumberOfGuessesInWonGames = Math.round((this.getTotalGuesses(rawData.replace(/7/g,"")) / this.numberOfWins) * 100) / 100
     }
 
     getNumberOfLosses(rawData: string) {
@@ -22,7 +24,7 @@ export class Statistics {
     }
 
     getTotalGuesses(rawData: string) {
-        var correctedData = rawData.replace("7", "6")
+        var correctedData = rawData.replace(/7/g, "6")
         var total = 0
         for (const c of correctedData) {
             total += Number(c)
