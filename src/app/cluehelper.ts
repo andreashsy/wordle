05e600da9a0a4b37cb2dd247249230ee59634a77
andreashsy <songyanghan+git@gmail.com>
2022-02-07 09:30:40 +0800

    EDIT: refactor clue helper

--- src/app/cluehelper.ts
+++ src/app/cluehelper.ts
@@ -1,93 +1,80 @@
 import { Prediction } from "./prediction"
 
 export class ClueHelper {
-    greenList: String[] = []
-    yellowList: String[] = []
-    greyList: String[] = []
-    ALL_LETTERS_CONCAT: String = "abcdefghijklmnopqrstuvwxyz"
-    whiteList: String[] = this.ALL_LETTERS_CONCAT.split("")
+  greenList: String[] = []
+  yellowList: String[] = []
+  greyList: String[] = []
+  ALL_LETTERS_CONCAT: String = "abcdefghijklmnopqrstuvwxyz"
+  whiteList: String[] = this.ALL_LETTERS_CONCAT.split("")
 
-    constructor(
-        public predictions:Prediction[]
-    ) {
-        
-    }
+  constructor(
+      public predictions:Prediction[]
+  ) {
+  }
 
-    updateAllLists() {
-        this.resetAllLists()
-        this.updateYellowList()
-        this.updateGreenList()
-        this.updateGreyList()
+  updateAllLists() {
+    this.resetAllLists()
+    for (var prediction of this.predictions) {
+      for (let i = 0; i < prediction.result.length; i++) {
+        const guessLetter = prediction.guess.charAt(i)
+        const resultLetter = prediction.result.charAt(i)
+        this.updateYellowList(guessLetter, resultLetter)
+        this.updateGreenList(guessLetter, resultLetter)
+        this.updateGreyList(guessLetter, resultLetter)
+      }
     }
+  }
 
-    updateYellowList() {
-        for (var prediction of this.predictions) {
-            for (let i = 0; i < prediction.result.length; i++) {
-                const guessLetter = prediction.guess.charAt(i)
-                const resultLetter = prediction.result.charAt(i)
-                if (resultLetter === "I") {
-                    if (this.whiteList.includes(guessLetter)) {
-                        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
-                    }
-                    if (!this.yellowList.includes(guessLetter)) {
-                        this.yellowList.push(guessLetter)
-                    }
-                }
-            }
-        }
+  updateYellowList(guessLetter: string, resultLetter: string) {
+    if (resultLetter === "I") {
+      if (this.whiteList.includes(guessLetter)) {
+        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
+      }
+      if (!this.yellowList.includes(guessLetter)) {
+        this.yellowList.push(guessLetter)
+      }
     }
+  }
 
-    updateGreenList() {
-        for (var prediction of this.predictions) {
-            for (let i = 0; i < prediction.result.length; i++) {
-                const guessLetter = prediction.guess.charAt(i)
-                const resultLetter = prediction.result.charAt(i)
-                if (resultLetter === "M") {
-                    if (this.whiteList.includes(guessLetter)) {
-                        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
-                    }
-                    if (this.yellowList.includes(guessLetter)) {
-                        this.yellowList = this.stringArrayRemove(this.yellowList, guessLetter)
-                    }
-                    if (!this.greenList.includes(guessLetter)) {
-                        this.greenList.push(guessLetter)
-                    }
-                }
-            }
-        }
+  updateGreenList(guessLetter: string, resultLetter: string) {
+    if (resultLetter === "M") {
+      if (this.whiteList.includes(guessLetter)) {
+        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
+      }
+      if (this.yellowList.includes(guessLetter)) {
+        this.yellowList = this.stringArrayRemove(this.yellowList, guessLetter)
+      }
+      if (!this.greenList.includes(guessLetter)) {
+        this.greenList.push(guessLetter)
+      }
     }
+  }
 
-    updateGreyList() {
-        for (var prediction of this.predictions) {
-            for (let i = 0; i < prediction.result.length; i++) {
-                const guessLetter = prediction.guess.charAt(i)
-                const resultLetter = prediction.result.charAt(i)
-                if (resultLetter === "N") {
-                    if (this.whiteList.includes(guessLetter)) {
-                        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
-                    }
-                    if (!this.greyList.includes(guessLetter)) {
-                        this.greyList.push(guessLetter)
-                    }
-                }
-            }
-        }
+  updateGreyList(guessLetter: string, resultLetter: string) {
+    if (resultLetter === "N") {
+      if (this.whiteList.includes(guessLetter)) {
+        this.whiteList = this.stringArrayRemove(this.whiteList, guessLetter)
+      }
+      if (!this.greyList.includes(guessLetter)) {
+        this.greyList.push(guessLetter)
+      }
     }
+  }
 
-    resetAllLists() {
-        this.greenList = []
-        this.yellowList = []
-        this.greyList = []
-        this.whiteList = this.ALL_LETTERS_CONCAT.split("")
-    }
+  resetAllLists() {
+    this.greenList = []
+    this.yellowList = []
+    this.greyList = []
+    this.whiteList = this.ALL_LETTERS_CONCAT.split("")
+  }
 
-    stringArrayRemove(array: String[], value: String) {
-        for (var i= 0; i < array.length; i++) {
-            if (array[i] === value) {
-                array.splice(i, 1)
-                i--
-            }
-        }
-        return array
+  stringArrayRemove(array: String[], value: String) {
+    for (var i= 0; i < array.length; i++) {
+      if (array[i] === value) {
+        array.splice(i, 1)
+        i--
+      }
     }
+    return array
+  }
 }
